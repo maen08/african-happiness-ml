c790517f0ab0af00130d5739cbea5b939483db07
maen08 <2001stany@gmail.com>
2021-05-11 18:28:07 +0300

    add: graphical presentation

--- data/model/predict.py
+++ data/model/predict.py
@@ -1,5 +1,11 @@
 import pandas as pd
 from matplotlib import pyplot as plt
+import seaborn
+
+
+
+
+seaborn.set()
 
 
 # load the file
@@ -10,15 +16,23 @@ data = pd.read_csv('africahappiness.csv')
 africa_data = data['Regional Indicator'] == 'Africa'
 africa = data[africa_data]
 
-''' check some important info
 
-print(africa.describe(include='all'))
-print(africa.isnull().sum())
-
-'''
+# if null is available, clean it
+# data_check = africa.isnull().sum()
+# africa = africa.dropna(axis=0)
+# print(africa.describe(include='all'))
 
 
 y = africa['Happiness Scores']
-x = africa.drop['Happiness Scores']
+x = africa[['Generosity', 'Perceptions of Corruption','Social Support', 'Healthy Life Expectancy', 'Regional Indicator']]
+    
 
-print(x)
+# ys = y.value.reshape(-1,1)
+x_matrix = x.iloc[:,0].values
+
+plt.scatter(x_matrix,y)
+plt.show()
+
+
+# print(x)
+# print(z)

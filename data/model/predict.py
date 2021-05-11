f075f7123a354976f8accb410652c4c460ebebfb
maen08 <2001stany@gmail.com>
2021-05-11 19:35:08 +0300

    update: features affecting HS

--- data/model/predict.py
+++ data/model/predict.py
@@ -3,9 +3,6 @@ from matplotlib import pyplot as plt
 import seaborn
 from sklearn.feature_selection import f_regression
 
-
-
-
 seaborn.set()
 
 
@@ -18,30 +15,34 @@ africa_data = data['Regional Indicator'] == 'Africa'
 africa = data[africa_data]
 
 
-# if null is available, clean it
-# data_check = africa.isnull().sum()
-# africa = africa.dropna(axis=0)
-# print(africa.describe(include='all'))
+''' if null is available, clean it:
 
+data_check = africa.isnull().sum()
+africa = africa.dropna(axis=0)
+print(africa.describe(include='all'))
 
+'''
 y = africa['Happiness Scores']
-x = africa[['Generosity', 'Perceptions of Corruption','Social Support', 'Healthy Life Expectancy', 'Regional Indicator']]
-    
-# 2d in 1d for plot
-x_matrix = x.iloc[:,0].values
-
-plt.scatter(x_matrix,y)
-# plt.show()
+x = africa[['Generosity', 'Perceptions of Corruption',
+            'Social Support', 'Healthy Life Expectancy', 'Regional Indicator']]
 
 
+''' 2d in 1d for plot '''
+x_matrix = x.iloc[:, 0].values
 
-Gen = africa.sort_values(by=['Generosity'], ascending=False).head()
-# print(Gen)
+plt.scatter(x_matrix, y)
+plt.show()
 
 
-X = x.values.reshape(-1,1)
-p_values = f_regression(X, y)[1]
+Generosity = africa.sort_values(by=['Generosity'], ascending=False).head()
 
-print(p_values)
-# print(x)
-# print(z)
+Perceptions_Corruption = africa.sort_values(
+    by=['Perceptions of Corruption'], ascending=False).head()
+
+Social_Support = africa.sort_values(by=['Social Support'], ascending=False).head()
+
+Life_Expectancy = africa.sort_values(
+    by=['Healthy Life Expectancy'], ascending=False).head()
+
+Regional_Indicator = africa.sort_values(by=['Regional Indicator'], ascending=False).head()
+
